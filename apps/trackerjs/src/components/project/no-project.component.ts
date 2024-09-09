9d2907254d358ed74d9c3b429c06ef72079add1e
josesaranda <josesaranda@gmail.com>
2024-09-09 09:27:18 +0200

    fix: typo

--- apps/trackerjs/src/components/project/no-project.component.ts
+++ apps/trackerjs/src/components/project/no-project.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
   template: `
     <div class="h-full w-full flex items-center justify-center">
       <h2 class="!text-xl">
-        There are no projects yet. Click on + to add you first project
+        There are no projects yet. Click on + to add your first project
       </h2>
     </div>
   `,
